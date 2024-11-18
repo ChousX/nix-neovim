7e7b45057b07e742c46b38e770aec64ccfffb37c
ChousX <gmanenergy@gmail.com>
2024-11-18 15:30:50 -0500

    snippets

--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,7 @@
   # Import all your configuration modules here
   imports = [ 
     ./bufferline.nix
+    ./snippets
     ./language
     ./plugins
     ./opts.nix
--- config/language/rust.nix
+++ config/language/rust.nix
@@ -10,5 +10,20 @@
     conform-nvim.settings.formatters_by_ft = {
       rust = [ "rustfmt" ];
     };
+    #rustaceanvim = {
+      #enable = true;
+      #settings = {
+        #tools.enable_clippy = true;
+        #server = {
+          #default_settings = {
+            #inlayHints.lifetimeElisionHints.enable = "always";
+            #rust-analyzer = {
+              #cargo = { allFeatures = true; };
+              #check = { command = "clippy"; };
+            #};
+          #};
+        #};
+      #};
+    #};
   };
 }
