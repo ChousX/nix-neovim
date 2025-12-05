3b412b1e1d2a778796f1697bc279968c7fe4d9c2
A. Garrett Gelwick III <gmanenergy@gmail.com>
2025-12-05 10:20:27 +0000

    trying to fix system freesing

--- config/language/rust.nix
+++ config/language/rust.nix
@@ -5,6 +5,10 @@
         enable = true;
         installCargo = true;
         installRustc = true;
+        settings = {
+          cargo.extraArgs = [ "-j" "2" ];
+          numThreads = 2;
+        };
       };
     };
     conform-nvim.settings.formatters_by_ft = {
